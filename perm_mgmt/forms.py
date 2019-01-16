05d4e684b4bb977fa3ad6c9d1a9fcf7101fb58c3
Arjun Kishore <arjunpk.fb@gmail.com>
2019-01-15 20:54:01 -0500

    Added Forms.py

---
+++ perm_mgmt/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class Create_Resource_Form(forms.Form):
+    continue
+
+class Make_Request_Form(forms.Form):
+    continue
+
+class Permission_Api_Form(forms.Form):
+    continue
